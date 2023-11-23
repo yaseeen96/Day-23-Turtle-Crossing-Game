a0674cf449169a4d336afa9c7b1bee1f9fe2a921
yaseeen96 <syedyaseen1539@gmail.com>
2023-11-23 13:16:49 +0530

    update image in readme

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+    screen.title("Turtle Crossing Game")
 
     car_manager.create_car()
     car_manager.move_cars()
